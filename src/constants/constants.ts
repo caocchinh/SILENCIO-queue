a40673c62dc2d82264353a5dd1956c1ce574b403
caocchinh <chinhcaocu@gmail.com>
2025-10-24 22:14:56 +0700

    update deadline

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const UNSUPPORT_TICKET_TYPE = ["Juggler"];
-export const SELECTION_DEADLINE = new Date("2025-10-24T23:00:00+07:00");
+export const SELECTION_DEADLINE = new Date("2025-10-27T11:00:00+07:00");
 export const EMAIL_BANNER =
   "https://drive.google.com/uc?id=1f_NVZZ3XSMnIWZr6J5jifmAEOa77ezVt";
 export const SILENCIO_HAUNTED_HOUSE_QUEUE_MANAGER_URL =
